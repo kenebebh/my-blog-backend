15fb370d2918f8e63c8c4d61cc66532d8d7f6052
kenebebh <kenebebhbanigo@gmail.com>
2025-09-16 22:42:01 +0100

    impletemented toggle for light and dark mode uing next themes

--- frontend/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import { Header } from "./header";
-import { Hero } from "./hero";
-import { Footer } from "./footer";
-import { PostsGrid } from "./posts-grid";
-
-export { Header, Hero, Footer, PostsGrid };
